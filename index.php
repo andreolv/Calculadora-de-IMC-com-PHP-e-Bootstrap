4d22fc2970146bf96888ab382116bc285a0aec56
André <andreolv.ar@gmail.com>
2020-08-13 08:30:58 -0300

    Definição dos metadados

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/main.css" rel="stylesheet" type="text/css">
+        <script type="text/javascript"src="js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript"src="js/bootstrap.min.js" ></script>
+        
+        <title>Calculadora de IMC</title>
+    </head>
+    <body>
+        <form name="formImc" action="index.php" method="POST">
+            <fieldset>
+                <legend>Calculadora de IMC</legend>
+            </fieldset>
+        </form>
+    </body>
+</html>
